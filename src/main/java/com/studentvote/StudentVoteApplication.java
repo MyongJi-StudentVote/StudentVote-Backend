cc6544be7055d74ca97bceaf6cb9813981faae51
LEEJaeHyeok97 <hazardous10@naver.com>
2024-10-29 11:49:26 +0900

    first commit

---
+++ src/main/java/com/studentvote/StudentVoteApplication.java
@@ -0,0 +1,13 @@
+package com.studentvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentVoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentVoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studentvote/StudentVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentVoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
