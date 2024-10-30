94c8d5ff0822d4f28febd571c3c595bcbf8f510b
이재혁 <hazardous10@naver.com>
2024-10-30 12:07:06 +0900

    chore: 초기설정 (#1)
    * chore: timezone seoul 설정
    
    * docs: 깃 템플릿, readme 추가
    
    * chore: 예외처리 및 응답 환경 설정

--- src/main/java/com/studentvote/StudentVoteApplication.java
+++ src/main/java/com/studentvote/StudentVoteApplication.java
@@ -1,10 +1,17 @@
 package com.studentvote;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentVoteApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StudentVoteApplication.class, args);
---
+++ src/main/java/com/studentvote/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.studentvote.domain.user.domain.repository;
+
+import com.studentvote.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
