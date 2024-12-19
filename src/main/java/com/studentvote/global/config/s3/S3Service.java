3fa3e86d73848571893c286f133856a4436df095
ssm00 <titan0724@naver.com>
2024-12-19 14:59:12 +0900

    feat : s3서비스 파일 생성, 후보자 등록 시 poster/유저id값 경로에 저장

---
+++ src/main/java/com/studentvote/global/config/s3/S3Service.java
@@ -0,0 +1,66 @@
+package com.studentvote.global.config.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.studentvote.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class S3Service {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private String createFileName(String originalFilename) {
+        return UUID.randomUUID().toString() + "_" + originalFilename;
+    }
+
+    public String uploadImageToS3(MultipartFile image) {
+        try {
+            String originName = image.getOriginalFilename();
+            String ext = originName.substring(originName.lastIndexOf("."));
+            String fileName = createFileName(ext);
+
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(image.getContentType());
+            metadata.setContentLength(image.getSize());
+
+            amazonS3.putObject(
+                    new PutObjectRequest(bucket, fileName, image.getInputStream(), metadata)
+                            .withCannedAcl(CannedAccessControlList.PublicRead)
+            );
+            return amazonS3.getUrl(bucket, fileName).toString();
+        } catch (IOException e) {
+            throw new RuntimeException("ImageUploadException");
+        }
+    }
+
+    public String uploadImageToS3(MultipartFile image, String fileName) {
+        try {
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(image.getContentType());
+            metadata.setContentLength(image.getSize());
+
+            amazonS3.putObject(
+                    new PutObjectRequest(bucket, fileName, image.getInputStream(), metadata)
+                            .withCannedAcl(CannedAccessControlList.PublicRead)
+            );
+            return amazonS3.getUrl(bucket, fileName).toString();
+        } catch (IOException e) {
+            throw new RuntimeException("ImageUploadException");
+        }
+    }
+
+}
