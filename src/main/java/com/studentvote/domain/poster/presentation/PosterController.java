e89ee20077d728783f27ae67ab5e20ef5ff760e6
ssm00 <titan0724@naver.com>
2024-12-19 17:10:52 +0900

    fix:엔드포인트 추가

--- src/main/java/com/studentvote/domain/poster/presentation/PosterController.java
+++ src/main/java/com/studentvote/domain/poster/presentation/PosterController.java
@@ -10,14 +10,19 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RequiredArgsConstructor
-@Controller
+@RestController
+@RequestMapping("api")
 public class PosterController {
 
     private final PosterService posterService;
 
+    @PostMapping("/poster/")
     public ResponseCustom<RegisterPosterResponse> registerPoster(@AuthenticationPrincipal CustomUserDetails userDetails, @ModelAttribute RegisterPosterRequest request) {
         RegisterPosterResponse registerPosterResponse = posterService.registerPoster(userDetails, request);
         return ResponseCustom.OK(registerPosterResponse);
