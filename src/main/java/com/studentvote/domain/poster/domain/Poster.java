7ac0733af08cd1d870b93077e28ef782abdb9e49
ssm00 <titan0724@naver.com>
2024-12-19 18:21:48 +0900

    feat:각 후보 별 게시물 등록 엔티티 생성

---
+++ src/main/java/com/studentvote/domain/poster/domain/Poster.java
@@ -0,0 +1,31 @@
+package com.studentvote.domain.poster.domain;
+
+
+import com.studentvote.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Poster {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    private String posterName;
+
+    @Column(length = 1000)
+    private String posterImage;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
