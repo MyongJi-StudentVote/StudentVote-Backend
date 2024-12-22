1c4b57511e15d7cf6a0bbed8c0ad7d607af1c725
이재혁 <hazardous10@naver.com>
2024-12-23 05:04:54 +0900

    fix: 단과대별 후보자 기본 정보 조회 기능 수정 (#44)
    * feat: 개표 결과 저장 기능 구현
    
    * feat: 개표 결과 조회 기능 구현
    
    * feat: 회원가입 시 소속 정보 받도록 수정
    
    * fix: 단과대별 후보자 기본 정보 조회 기능 수정

--- src/main/java/com/studentvote/domain/candidateInfo/domain/CandidateInfoRepository.java
+++ src/main/java/com/studentvote/domain/candidateInfo/domain/CandidateInfoRepository.java
@@ -14,7 +14,13 @@ public interface CandidateInfoRepository extends JpaRepository<CandidateInfo, Lo
             "join c.user u " +
             "join u.governance g " +
             "where u.id = :userId " +
-            "and g.governanceType = :governanceType")
-    List<CandidateInfo> findAllCandidateByGovernanceType(@Param("userId") Long userId, @Param("governanceType") String governanceType);
+            "and g.id = :governanceId")
+    List<CandidateInfo> findAllCandidateByGovernanceType(@Param("userId") Long userId, @Param("governanceId") String governanceId);
 
+
+    @Query("select c from CandidateInfo c "
+            + "join c.user u "
+            + "join u.governance g "
+            + "where g.id = :governanceId")
+    List<CandidateInfo> findAllByGovernanceId(String governanceId);
 }
